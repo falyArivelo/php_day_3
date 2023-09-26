cbb89f6f55d077bec2d98d429c2ddbdba98ed85d
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2023-09-26 19:56:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
 <?php
 include('fonction.php');
 
-if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['age'])){
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $age = $_POST['age'];
+// if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['age'])){
+//     $nom = $_POST['nom'];
+//     $prenom = $_POST['prenom'];
+//     $age = $_POST['age'];
 
-    echo "Salut ".$nom." ".$prenom." vous êtes ".isMajor($age);
-}
-else{
-    $note = "";
-    $prenom = "";
-    $age = "";
+//     echo "Salut ".$nom." ".$prenom." vous êtes ".isMajor($age);
+// }
+// else{
+//     $nom = "";
+//     $prenom = "";
+//     $age = "";
+// }
+
+$nom = $_POST['nom'] ?? '' ;
+$prenom = $_POST['prenom'] ?? '';
+$age = $_POST['age'] ?? '';
+if(!empty($nom)){
+    echo sprintf("Salut %s %s vous êtes %s ", $nom, $prenom, isMajor($age));
+    // echo "Salut ".$nom." ".$prenom." vous êtes ".isMajor($age);    
 }
 
 ?>
